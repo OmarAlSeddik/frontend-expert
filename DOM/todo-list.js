0dd1742dfcdb9bbe7377525aa2f45c6a607f64a8
unknown <omaralseddik@gmail.com>
2022-11-09 18:51:21 +0200

    todo-list

---
+++ DOM/todo-list.js
@@ -0,0 +1,33 @@
+const wrapper = document.getElementById("wrapper");
+const todoInput = document.getElementById("todo-input");
+const addButton = document.getElementById("add-button");
+const todoList = document.getElementById("todo-list");
+
+const handleInput = (event) => {
+  text = event.target.value;
+  addButton.disabled = text.length > 0 ? false : true;
+};
+
+const handleDelete = (event) => {
+  event.target.parentNode.remove();
+};
+
+const handleClick = () => {
+  const todoItem = document.createElement("li");
+  const text = document.createElement("h2");
+  text.textContent = todoInput.value;
+  todoItem.appendChild(text);
+
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "X";
+  deleteButton.classList.add("delete-button");
+  deleteButton.addEventListener("click", handleDelete);
+  todoItem.appendChild(deleteButton);
+
+  todoList.appendChild(todoItem);
+  todoInput.value = "";
+  addButton.disabled = true;
+};
+
+todoInput.addEventListener("input", handleInput);
+addButton.addEventListener("click", handleClick);
